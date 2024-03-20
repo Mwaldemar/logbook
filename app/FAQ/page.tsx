b934aa50458bb5629145246376a46e5ef1d2b69b
Mikkel W <mijo@trendhim.com>
2024-03-20 15:31:18 +0100

    Updated accordion without hook

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -1,34 +1,60 @@
-"use client"
-import React from 'react'
-import Accordion from '../components/accordion/accordion';
+import React, { useMemo } from 'react'
+import { Accordion } from '../components/accordion/accordion';
 import styles from './faq.module.scss'
+import CloseButtonSVG from '../svg/CloseButtonSvg'
+import ArrowSvg from '../svg/ArrowSvg';
+import clsx from 'clsx';
 
 const items = [
   {
+    id: "1",
     title: "Accordion Item 1",
     content: "Content for Accordion Item 1",
   },
   {
+    id: "2",
     title: "Accordion Item 2",
     content: "Content for Accordion Item 2",
   },
   {
+    id: "3",
     title: "Accordion Item 3",
     content: "Content for Accordion Item 3",
   },
   {
+    id: "4",
     title: "Accordion Item 4",
     content: "Content for Accordion Item 4",
   },
 ];
 
 const FAQ = () => {
-    return (
-        <>
-          <h2 className={styles.title}>FAQ</h2>
-          <Accordion items={items} />
-        </>
-    )
+  const trigger = useMemo(() => {
+    return items.map(item => (
+      <div key={item.id} className={styles.ruleTrigger}>
+        <p>{item.title}</p>
+        <span className={styles.arrowSvg}>{ArrowSvg}</span>
+      </div>
+    ))
+  }, [])
+  return (
+    <>
+      <h2 className={styles.title}>FAQ</h2>
+      {trigger.map((item, index) => (
+        <Accordion
+          key={index}
+          trigger={item}
+          id={items[index].id}
+          SVG={CloseButtonSVG}
+        >
+          <div className={styles.accordionContents}>
+            <h2 className={clsx(styles.title, styles.mobileSidepanel)}>{items[index].title}</h2>
+            <p className={clsx(styles.content, styles.mobileSidepanel, styles.desktopSidepanel)}>{items[index].content}</p>
+          </div>
+        </Accordion>
+      ))}
+    </>
+  )
 }
 
 export default FAQ;
--- app/components/accordion/accordion.tsx
+++ app/components/accordion/accordion.tsx
@@ -1,89 +1,30 @@
-import React, { FC, useState } from "react";
+import React, { FC, ReactNode } from "react";
 import clsx from 'clsx';
 import styles from './accordion.module.scss';
 
 interface AccordionProps {
-    items: { title: string; content: string }[];
+    className?: string
+    trigger: ReactNode;
+    children: ReactNode;
+    id: string;
+    desktopSidepanel?: boolean;
+    mobileSidepanel?: boolean;
+    SVG?: ReactNode
 }
 
-const Accordion: FC<AccordionProps> = ({ items }) => {
-    const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-    const handleButtonClick = (index: number) => {
-        setOpenIndex(openIndex === index ? null : index);
-    };
-
-    const handleClose = () => {
-        setOpenIndex(null)
-    }
-
+export const Accordion: FC<AccordionProps> = ({ className, trigger, children, id, desktopSidepanel, mobileSidepanel, SVG }) => {
     return (
-        <div className={clsx(styles.accordionWrapper)}>
-            <div className={styles.backdrop} onClick={handleClose}></div>
-            {items.map((item, index) => (
-                <div key={index}>
-                    <input
-                        type="checkbox"
-                        id={`accordion-checkbox-${index}`}
-                        className={styles.checkbox}
-                        checked={openIndex === index}
-                        onChange={() => setOpenIndex(openIndex)}
-                    />
-                    <label
-                        htmlFor={`accordion-checkbox-${index}`}
-                        className={clsx(styles.label, {
-                            [styles.active]: openIndex === index,
-                        })}
-                        onClick={() => handleButtonClick(index)}
-                    >
-                        <span>{item.title}</span>
-                        <button
-                            type='button'
-                        >
-                            <div>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    width="24"
-                                    height="24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className={openIndex === index ? styles.rotated : ''}
-                                >
-                                    <path d="M9 18l6-6-6-6" />
-                                </svg>
-                            </div>
-                        </button>
-                    </label>
-                    <div
-                        className={clsx(styles.content)}
-                    >
-                        <button className={styles.closeButton} onClick={() => handleButtonClick(index)}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className={styles.closeIcon}
-                                style={{ zIndex: 1000 }}
-                            >
-                                <path d="M18 6L6 18M6 6l12 12"></path>
-                            </svg>
-                        </button>
-                        <p className={styles.accordionContent}>{item.content}</p>
-                    </div>
-                </div>
-            ))}
+        <div className={clsx(styles.accordionWrapper, className)}>
+            <input type="checkbox" id={id} className={styles.checkbox} />
+            <label htmlFor={id} className={styles.trigger}>
+                {trigger}
+            </label>
+            <div className={clsx(styles.content, desktopSidepanel && styles.desktopSidepanel, mobileSidepanel && styles.mobileSidepanel)}>
+                <label htmlFor={id} className={styles.close}>
+                    {SVG}
+                </label>
+                {children}
+            </div>
         </div>
-    );
-};
-
-export default Accordion;
+    )
+}
