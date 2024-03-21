29ed5621420eea71919c121dc966bf0024abc8b0
Mikkel W <mijo@trendhim.com>
2024-03-21 14:21:51 +0100

    Accordion done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,25 +1,10 @@
 import React, { useMemo } from 'react'
 import styles from './accordionTest.module.scss'
-import { Accordionv2 } from '../components/accordionv2/accordionv2'
-
-export const BurgAfAccordion = () => {
-    const trigger = useMemo(() => {
-        return <div className={styles.ruleTrigger}>
-            <p>Accordion Title</p>
-        </div>
-    }, [])
-    return <Accordionv2 className={styles.rules} trigger={trigger}>
-        <div className={styles.ruleContent}>
-            Hej Mikkel her er indholdet af din accordion
-        </div>
-    </Accordionv2>
-}
 
 const AboutPage = () => {
     return (
         <>
             <p>Just to play around with some navigation and a placeholder if i decide to add more.</p>
-            <BurgAfAccordion />
         </>
     )
 }
--- app/components/accordion/accordion.tsx
+++ app/components/accordion/accordion.tsx
@@ -16,13 +16,16 @@ export const Accordion: FC<AccordionProps> = ({ className, trigger, children, id
     return (
         <div className={clsx(styles.accordionWrapper, className)}>
             <input type="checkbox" id={id} className={styles.checkbox} />
+            <label htmlFor={id} className={styles.backdrop}></label>
             <label htmlFor={id} className={styles.trigger}>
                 {trigger}
             </label>
             <div className={clsx(styles.content, desktopSidepanel && styles.desktopSidepanel, mobileSidepanel && styles.mobileSidepanel)}>
+            <div className={styles.borderBottom}>
                 <label htmlFor={id} className={styles.close}>
                     {SVG}
                 </label>
+                </div>
                 {children}
             </div>
         </div>
